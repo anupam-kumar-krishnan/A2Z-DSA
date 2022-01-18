f1dd332daa88cd3d559fcf21593673d5b1de4a61
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2022-01-18 16:26:45 +0530

    Delete icecream directory

--- icecream/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from home.models import Contact
-
-# Register your models here.
-admin.site.register(Contact)
--- icecream/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Contact(models.Model):
-    name = models.CharField(max_length=122)
-    email = models.CharField(max_length=122)
-    phone = models.CharField(max_length=12)
-    desc = models.TextField()
-    date = models.DateTimeField()
