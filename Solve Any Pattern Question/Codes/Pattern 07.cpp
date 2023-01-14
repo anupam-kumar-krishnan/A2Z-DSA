ddbfe73f70c3c66200ca83583d2f2c92f93069d8
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2023-01-15 00:40:11 +0530

    Pattern 07

---
+++ Solve Any Pattern Question/Codes/Pattern 07.cpp
@@ -0,0 +1,47 @@
+/* Tree Pattern */
+
+/*
+
+     *
+    ***
+   *****
+  *******
+ *********
+
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void print(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        // space
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        // star
+        for (int j = 0; j < 2 * i + 1; j++)
+            cout << "*";
+        // space
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++)
+    {
+        int n;
+        cin >> n;
+        print(n);
+    }
+}
