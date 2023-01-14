f8d3857135a146883219cbfd08ef1c88a04f0a78
Anupam Kumar Krishnan <69143883+anupam-kumar-krishnan@users.noreply.github.com>
2023-01-14 23:52:08 +0530

    Delete ecom directory

--- ecom/api/category/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from .models import Category
-
-
-class CategorySerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Category
-        fields = ('name', 'description')
--- ecom/api/user/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import CustomUser
-# Register your models here.
-
-admin.site.register(CustomUser)
